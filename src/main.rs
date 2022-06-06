2faabe8a470ed3fc9e98ab01a955a411ce8db829
Nikon <47792796+INikonI@users.noreply.github.com>
2022-06-06 22:27:07 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,88 @@
+use chrono::{prelude::Local, Timelike};
 use discord_rich_presence::{
     activity::{Activity, Assets, Timestamps},
     DiscordIpc, DiscordIpcClient,
 };
-use precord_core::Features;
 use std::{
-    thread,
+    thread::sleep,
     time::{Duration, SystemTime, UNIX_EPOCH},
 };
 use sys_info::loadavg as cpu_usage;
-use sysinfo::{ProcessorExt, RefreshKind, SystemExt};
-use chrono::{prelude::Local, Timelike};
 
+#[cfg(target_os = "windows")]
 fn main() {
-    let precord_system = precord_core::System::new(Features::CPU_FREQUENCY, None).unwrap();
-    let mut sysinfo_system =
-        sysinfo::System::new_with_specifics(RefreshKind::new().with_cpu().with_memory());
+    use std::collections::HashMap;
+    use sysinfo::{ProcessorExt, RefreshKind, SystemExt};
+    use wmi::{COMLibrary, Variant, WMIConnection};
 
-    let physical_cores = sysinfo_system
+    // info providers
+    let wmi_con = WMIConnection::new(COMLibrary::new().unwrap().into()).unwrap();
+    let mut sysinfo_system = sysinfo::System::new_with_specifics(RefreshKind::new().with_memory());
+
+    // precompute some info
+    let max_freq: f64 = {
+        let result: Vec<HashMap<String, Variant>> = wmi_con
+            .raw_query("SELECT MaxClockSpeed FROM Win32_Processor")
+            .unwrap();
+        if let Variant::UI4(val) = result
+            .into_iter()
+            .next()
+            .unwrap()
+            .get("MaxClockSpeed")
+            .unwrap()
+        {
+            *val as f64
+        } else {
+            0.0
+        }
+    };
+    let physical_cores: usize = sysinfo_system
         .physical_core_count()
         .expect("Failed to get physical core count");
-    let logical_cores = sysinfo_system.processors().len();
-    let cpu_brand = sysinfo_system.global_processor_info().brand().to_owned();
-    let total_memory = (sysinfo_system.total_memory() as f64 / 1024_f64 / 1024_f64).round();
+    let logical_cores: usize = sysinfo_system.processors().len();
+    let cpu_brand: String = sysinfo_system.global_processor_info().brand().to_owned();
+    let total_memory: f64 = (sysinfo_system.total_memory() as f64 / 1024_f64 / 1024_f64).round();
 
+    // Discord RPC init
     let mut drpc = DiscordIpcClient::new("983347731823210567").expect("Failed to create client");
 
     println!("Connecting client...");
     while drpc.connect().is_err() {
         eprintln!("Failed to connect. Trying to reconnect...");
-        thread::sleep(Duration::from_millis(100));
+        sleep(Duration::from_millis(100));
     }
     println!("Client connected!");
 
+    // main loop
     loop {
-        let freqs = precord_system
-            .cpus_frequency()
-            .expect("Failed to get cpus frequency");
-        let used_memory = (sysinfo_system.used_memory() as f64 / 1024_f64 / 1024_f64).round();
-        // let date_now = Local::now();
+        let used_memory: f64 = (sysinfo_system.used_memory() as f64 / 1024_f64 / 1024_f64).round();
+        let current_freq: f64 = {
+            let result: Vec<HashMap<String, Variant>> = wmi_con.raw_query("SELECT PercentProcessorPerformance FROM Win32_PerfFormattedData_Counters_ProcessorInformation WHERE Name LIKE \"_Total\"").unwrap();
+            let percent_perfomance: f64 = if let Variant::UI8(val) = result
+                .into_iter()
+                .next()
+                .unwrap()
+                .get("PercentProcessorPerformance")
+                .unwrap()
+            {
+                *val as f64
+            } else {
+                0.0
+            };
+            percent_perfomance * max_freq / 100_f64 / 1000_f64
+        };
+
         match drpc.set_activity(
             Activity::new()
                 .details(&format!(
                     "CPU: {:.0}% | RAM: {}/{} GB",
-                    cpu_usage().unwrap().one * 100.0,
+                    cpu_usage().expect("Failed to get cpu usage").one * 100_f64,
                     used_memory,
                     &total_memory
                 ))
                 .state(&format!(
                     "{:.2} GHz | {}/{} Cores | {}",
-                    freqs.iter().sum::<f32>() / freqs.len() as f32 / 1024_f32,
-                    &physical_cores,
-                    &logical_cores,
-                    &cpu_brand
+                    current_freq, &physical_cores, &logical_cores, &cpu_brand
                 ))
                 .assets(Assets::new().large_image("shark"))
                 .timestamps(
@@ -60,7 +91,7 @@ fn main() {
                             .duration_since(UNIX_EPOCH)
                             .unwrap()
                             .as_millis() as i64
-                        - (Local::now().time().num_seconds_from_midnight() as i64 * 1000),
+                            - (Local::now().time().num_seconds_from_midnight() as i64 * 1000),
                     ),
                 ),
         ) {
@@ -68,7 +99,67 @@ fn main() {
             Err(_) => eprintln!("Activity update failed"),
         }
 
-        thread::sleep(Duration::from_millis(4001));
+        sleep(Duration::from_millis(4001));
+        sysinfo_system.refresh_memory();
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+fn main() {
+    use sysinfo::{CpuExt, RefreshKind, SystemExt};
+
+    // info providers
+    let mut sysinfo_system = sysinfo::System::new_with_specifics(RefreshKind::new().with_memory());
+
+    // precompute some info
+    let physical_cores: usize = sysinfo_system.physical_core_count().expect("Failed to get physical core count");
+    let logical_cores: usize = sysinfo_system.cpus().len();
+    let cpu_brand: String = sysinfo_system.global_cpu_info().brand().to_owned();
+    let total_memory: f64 = (sysinfo_system.total_memory() as f64 / 1024_f64 / 1024_f64).round();
+
+    // Discord RPC init
+    let mut drpc = DiscordIpcClient::new("983347731823210567").expect("Failed to create client");
+
+    println!("Connecting client...");
+    while drpc.connect().is_err() {
+        eprintln!("Failed to connect. Trying to reconnect...");
+        sleep(Duration::from_millis(100));
+    }
+    println!("Client connected!");
+
+    // main loop
+    loop {
+        let used_memory: f64 = (sysinfo_system.used_memory() as f64 / 1024_f64 / 1024_f64).round();
+        let current_freq: f64 = sysinfo_system.global_cpu_info().frequency() as f64 / 1000_f64;
+
+        match drpc.set_activity(
+            Activity::new()
+                .details(&format!(
+                    "CPU: {:.0}% | RAM: {}/{} GB",
+                    cpu_usage().expect("Failed to get cpu usage").one * 100.0,
+                    used_memory,
+                    &total_memory
+                ))
+                .state(&format!(
+                    "{:.2} GHz | {}/{} Cores | {}",
+                    current_freq, &physical_cores, &logical_cores, &cpu_brand
+                ))
+                .assets(Assets::new().large_image("shark"))
+                .timestamps(
+                    Timestamps::new().start(
+                        SystemTime::now()
+                            .duration_since(UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis() as i64
+                            - (Local::now().time().num_seconds_from_midnight() as i64 * 1000),
+                    ),
+                ),
+        ) {
+            Ok(_) => println!("Activity updated"),
+            Err(_) => eprintln!("Activity update failed"),
+        }
+
+        sleep(Duration::from_millis(4001));
         sysinfo_system.refresh_memory();
     }
 }
