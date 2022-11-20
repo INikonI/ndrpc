7f8e1144c45b81f4641f5d8546bda1238425acbc
INikonI <anti-obolll@mail.ru>
2022-11-20 20:49:26 +0500

    adjust printing

--- src/printing.rs
+++ src/printing.rs
@@ -8,8 +8,8 @@ use crossterm::{
 use serde::Deserialize;
 
 pub enum PrintRow {
-    Version = 3,
-    Copyright = 5,
+    Version = 2,
+    Copyright = 4,
     NewVersionNotify = 9,
     ClientStatus = 11,
     ActivityStatus = 12,
@@ -19,27 +19,33 @@ pub enum PrintRow {
 }
 
 pub fn print_header() {
+    let mut stdout = stdout();
     _ = execute!(
-        stdout(),
+        stdout,
         SetTitle("ndrpc"),
         SetSize(80, 17),
         cursor::Hide,
-        Print(
-            r"            _
-           | |
- _ __    __| | _ __  _ __    ___
-| '_ \  / _` || '__|| '_ \  / __|
-| | | || (_| || |   | |_) || (__
-|_| |_| \__,_||_|   | .__/  \___|
-                    | |
-                    |_|"
-            .with(Color::Blue)
-            .bold()
-        ),
-        cursor::MoveTo(37, PrintRow::Copyright as u16),
-        Print("Copyright (c) 2022 INikonI".with(Color::Blue)),
-        cursor::MoveTo(37, PrintRow::Version as u16),
-        Print(format!("Version {}", env!("CARGO_PKG_VERSION")).with(Color::Blue)),
+        Clear(ClearType::All)
+    );
+    let ansi_title: Vec<&str> = r"
+███    ██  ██████   ██████   ██████    ██████
+████   ██  ██   ██  ██   ██  ██   ██  ██
+██ ██  ██  ██   ██  ██████   ██████   ██
+██  ██ ██  ██   ██  ██   ██  ██       ██
+██   ████  ██████   ██   ██  ██        ██████".split('\n').collect();
+    for (i, line) in ansi_title.iter().enumerate() {
+        _ = execute!(
+            stdout,
+            cursor::MoveTo(1, i as u16),
+            Print(line.with(Color::Blue))
+        );
+    }
+    _ = execute!(
+        stdout,
+        cursor::MoveTo(50, PrintRow::Copyright as u16),
+        Print("Copyright (c) 2022 INikonI".with(Color::Blue).bold()),
+        cursor::MoveTo(50, PrintRow::Version as u16),
+        Print(format!("Version {}", env!("CARGO_PKG_VERSION")).with(Color::Blue).bold()),
     );
 }
 
@@ -59,13 +65,14 @@ pub fn print_new_version_notify() {
     if latest_version != env!("CARGO_PKG_VERSION") {
         _ = execute!(
             stdout(),
-            cursor::MoveTo(0, PrintRow::NewVersionNotify as u16),
+            cursor::MoveTo(1, PrintRow::NewVersionNotify as u16),
             Print(
                 format!(
                     "New version available! https://github.com/INikonI/ndrpc/releases/tag/v{}",
                     latest_version
                 )
-                .with(Color::Cyan),
+                .with(Color::Cyan)
+                .bold(),
             )
         );
     }
@@ -74,7 +81,7 @@ pub fn print_new_version_notify() {
 pub fn print_binds_custom_presence() {
     _ = execute!(
         stdout(),
-        cursor::MoveTo(0, PrintRow::Binds as u16),
+        cursor::MoveTo(1, PrintRow::Binds as u16),
         Print("Press ".with(Color::Grey)),
         Print("CTRL+R".with(Color::Black).on(Color::Grey)),
         Print(" to reload preset(s)".with(Color::Grey))
@@ -84,7 +91,7 @@ pub fn print_binds_custom_presence() {
 pub fn print_client_status(text: StyledContent<&str>) {
     _ = execute!(
         stdout(),
-        cursor::MoveTo(0, PrintRow::ClientStatus as u16),
+        cursor::MoveTo(1, PrintRow::ClientStatus as u16),
         Clear(ClearType::CurrentLine),
         Print("CLIENT STATUS: ".bold()),
         Print(text)
@@ -94,7 +101,7 @@ pub fn print_client_status(text: StyledContent<&str>) {
 pub fn print_activity_status(text: StyledContent<&str>) {
     _ = execute!(
         stdout(),
-        cursor::MoveTo(0, PrintRow::ActivityStatus as u16),
+        cursor::MoveTo(1, PrintRow::ActivityStatus as u16),
         Clear(ClearType::CurrentLine),
         Print("ACTIVITY STATUS: ".bold()),
         Print(text)
@@ -105,7 +112,7 @@ pub fn print_info(text: StyledContent<&str>) {
     let mut stdout = stdout();
     _ = execute!(
         stdout,
-        cursor::MoveTo(0, PrintRow::Info as u16),
+        cursor::MoveTo(1, PrintRow::Info as u16),
         Clear(ClearType::CurrentLine),
         Print("INFO: ".bold()),
         Print(text)
@@ -113,7 +120,7 @@ pub fn print_info(text: StyledContent<&str>) {
     sleep(Duration::from_secs(3));
     _ = execute!(
         stdout,
-        cursor::MoveTo(0, PrintRow::Info as u16),
+        cursor::MoveTo(1, PrintRow::Info as u16),
         Clear(ClearType::CurrentLine)
     );
 }
@@ -122,7 +129,7 @@ pub fn print_error(text: &str) {
     let mut stdout = stdout();
     _ = execute!(
         stdout,
-        cursor::MoveTo(0, PrintRow::Error as u16),
+        cursor::MoveTo(1, PrintRow::Error as u16),
         Clear(ClearType::CurrentLine),
         Print("ERROR: ".with(Color::Red).bold()),
         Print(text)
@@ -130,7 +137,7 @@ pub fn print_error(text: &str) {
     sleep(Duration::from_secs(3));
     _ = execute!(
         stdout,
-        cursor::MoveTo(0, PrintRow::Error as u16),
+        cursor::MoveTo(1, PrintRow::Error as u16),
         Clear(ClearType::CurrentLine)
     );
 }
